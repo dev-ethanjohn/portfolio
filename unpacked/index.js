c06c43316a3415ccbe184afcd006fcc51bde550b
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-05-05 16:09:06 +0800

    feat: fade out on scroll to main

--- unpacked/index.js
+++ unpacked/index.js
@@ -1,6 +1,3 @@
-import profileImage from "./src/assets/profile-image.jpg";
 import "./src/sass/main.scss";
-
-document.querySelector(".hero__profile-image").src = profileImage;
 
 console.log("Hello");
